fa6fda0e63ec0c9fb1683818bfd3b35cbd15b603
Elijah <xrealntrue16x@gmail.com>
2024-11-29 23:06:44 -0500

    SideBar Almost done

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import "./Sidebar.css"; // Assuming you have a separate CSS file for styling
 import stockProfile from "../../assets/stockProfilePic.jpg";
+import githubIcon from "../../assets/githublogo.jpg";
+import linkedInIcon from "../../assets/linkedinicon.png";
 
 function Sidebar() {
   // Custom class for active links
@@ -14,6 +16,7 @@ function Sidebar() {
       <img src={stockProfile} className="sidebar__image"></img>
 
       <h2 className="sidebar__title">Elijah Williams</h2>
+      <p className="sidebar__subtitle">Full-Stack Developer</p>
       {/* Sidebar Navigation */}
       <nav className="sidebar__list">
         <NavLink to="/" className={customClass}>
@@ -29,6 +32,29 @@ function Sidebar() {
           Contact
         </NavLink>
       </nav>
+
+      <div className="social__icons">
+        <ul className="social__icons">
+          <li className="social__icon-github">
+            <a
+              href="https://github.com/yourprofile"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={githubIcon} alt="GitHub" />
+            </a>
+          </li>
+          <li className="social__icon-linkedin">
+            <a
+              href="https://linkedin.com/in/yourprofile"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={linkedInIcon} alt="LinkedIn" />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
