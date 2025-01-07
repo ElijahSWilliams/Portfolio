8f33f797e94df26123072ac1899ce409b95a0687
Elijah <xrealntrue16x@gmail.com>
2025-01-07 16:25:40 -0500

    Button Added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,8 +3,9 @@ import "./Home.css";
 function Home() {
   return (
     <div className="home">
-      <h1 className="home__title">Hi, I'm Elijah WIlliams. </h1>
-      <p className="home__description">I am a Full-Stack Software Engineer</p>
+      <h1 className="home__title">Hi, I'm Elijah Williams. </h1>
+      <p className="home__description"> Full-Stack Software Engineer</p>
+      <button className="home__button">Get To Know Me</button>
     </div>
   );
 }
